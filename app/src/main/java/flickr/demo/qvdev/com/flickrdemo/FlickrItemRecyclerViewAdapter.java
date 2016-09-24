7eeef324a8b87bf1df2124abc5c133f8cc84a45f
QVDev <feedbackandroid@gmail.com>
2016-09-24 15:17:41 +0300

    Remove holder item as not used anymore

--- app/src/main/java/flickr/demo/qvdev/com/flickrdemo/FlickrItemRecyclerViewAdapter.java
+++ app/src/main/java/flickr/demo/qvdev/com/flickrdemo/FlickrItemRecyclerViewAdapter.java
@@ -27,7 +27,6 @@ class FlickrItemRecyclerViewAdapter
 
     @Override
     public void onBindViewHolder(final FlickrViewHolder holder, int position) {
-        holder.mItem = mItems.get(position);
         holder.mIdView.setText(mItems.get(position).id);
         holder.mContentView.setText(mItems.get(position).content);
     }
