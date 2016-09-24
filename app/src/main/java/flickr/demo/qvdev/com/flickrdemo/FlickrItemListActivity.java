b847b2bd3dc7f19f882337c2f93144e8f58d50ed
QVDev <feedbackandroid@gmail.com>
2016-09-24 14:44:00 +0300

    Move onClick outside the adapter reducing dependencies

--- app/src/main/java/flickr/demo/qvdev/com/flickrdemo/FlickrItemListActivity.java
+++ app/src/main/java/flickr/demo/qvdev/com/flickrdemo/FlickrItemListActivity.java
@@ -1,11 +1,14 @@
 package flickr.demo.qvdev.com.flickrdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import java.util.List;
 
 import flickr.demo.qvdev.com.flickrdemo.dummy.DummyContent;
 
@@ -19,6 +22,10 @@ import flickr.demo.qvdev.com.flickrdemo.dummy.DummyContent;
  */
 public class FlickrItemListActivity extends AppCompatActivity {
 
+    private boolean mIsTwoPane;
+    private List<DummyContent.DummyItem> mFlickrItems = DummyContent.ITEMS;
+    private RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,23 +35,37 @@ public class FlickrItemListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        View recyclerView = findViewById(R.id.flickritem_list);
-        assert recyclerView != null;
-        setupRecyclerView((RecyclerView) recyclerView);
+        mRecyclerView = (RecyclerView) findViewById(R.id.flickritem_list);
+        assert mRecyclerView != null;
+        setupRecyclerView(mRecyclerView);
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        boolean isTwoPane = false;
-        if (findViewById(R.id.flickritem_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
-            isTwoPane = true;
-        }
-
-        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS, getSupportFragmentManager(), isTwoPane));
+        // For larger screen w300 it will have a detail view in same screen
+        mIsTwoPane = findViewById(R.id.flickritem_detail_container) != null;
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(mFlickrItems));
     }
 
 
+    public void flickrItemClicked(View view) {
+        // Get the matching FlickrItem from the recyclerView
+        DummyContent.DummyItem item = mFlickrItems.get(mRecyclerView.getChildAdapterPosition(view));
+
+        if (mIsTwoPane) {
+            // Larger screens w300 will replace the fragment for detail view
+            Bundle arguments = new Bundle();
+            arguments.putString(FlickrItemDetailFragment.ARG_ITEM_ID, item.id);
+            FlickrItemDetailFragment fragment = new FlickrItemDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.flickritem_detail_container, fragment)
+                    .commit();
+        } else {
+            // Smaller screens < w300 will open a new activity for detail view
+            Intent intent = new Intent(this, FlickrItemDetailActivity.class);
+            intent.putExtra(FlickrItemDetailFragment.ARG_ITEM_ID, item.id);
+
+            startActivity(intent);
+        }
+    }
 }
--- app/src/main/java/flickr/demo/qvdev/com/flickrdemo/SimpleItemRecyclerViewAdapter.java
+++ app/src/main/java/flickr/demo/qvdev/com/flickrdemo/SimpleItemRecyclerViewAdapter.java
@@ -1,9 +1,5 @@
 package flickr.demo.qvdev.com.flickrdemo;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,14 +12,10 @@ import flickr.demo.qvdev.com.flickrdemo.dummy.DummyContent;
 class SimpleItemRecyclerViewAdapter
         extends RecyclerView.Adapter<FlickrViewHolder> {
 
-    private final List<DummyContent.DummyItem> mValues;
-    private final FragmentManager mFragmentManager;
-    private final boolean mIsTwoPane;
+    private final List<DummyContent.DummyItem> mItems;
 
-    SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items, FragmentManager fragmentManager, boolean isTwoPane) {
-        mValues = items;
-        mFragmentManager = fragmentManager;
-        mIsTwoPane = isTwoPane;
+    SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
+        mItems = items;
     }
 
     @Override
@@ -35,34 +27,13 @@ class SimpleItemRecyclerViewAdapter
 
     @Override
     public void onBindViewHolder(final FlickrViewHolder holder, int position) {
-        holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
-
-        holder.mView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mIsTwoPane) {
-                    Bundle arguments = new Bundle();
-                    arguments.putString(FlickrItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
-                    FlickrItemDetailFragment fragment = new FlickrItemDetailFragment();
-                    fragment.setArguments(arguments);
-                    mFragmentManager.beginTransaction()
-                            .replace(R.id.flickritem_detail_container, fragment)
-                            .commit();
-                } else {
-                    Context context = v.getContext();
-                    Intent intent = new Intent(context, FlickrItemDetailActivity.class);
-                    intent.putExtra(FlickrItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
-
-                    context.startActivity(intent);
-                }
-            }
-        });
+        holder.mItem = mItems.get(position);
+        holder.mIdView.setText(mItems.get(position).id);
+        holder.mContentView.setText(mItems.get(position).content);
     }
 
     @Override
     public int getItemCount() {
-        return mValues.size();
+        return mItems.size();
     }
 }
